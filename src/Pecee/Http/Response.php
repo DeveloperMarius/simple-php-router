1a2d707974f6798e845a60dd399a995ef04b8566
Jérémie Arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2023-01-01 22:13:21 +0100

    Fix PHP8.1 deprecation of non-nullable parameters

--- src/Pecee/Http/Response.php
+++ src/Pecee/Http/Response.php
@@ -107,7 +107,7 @@ class Response
      * @throws InvalidArgumentException
      */
     #[NoReturn]
-    public function json(mixed $value, ?int $options = null, int $dept = 512): void
+    public function json(mixed $value, int $options = 0, int $dept = 512): void
     {
         if (($value instanceof JsonSerializable) === false && is_array($value) === false) {
             throw new InvalidArgumentException('Invalid type for parameter "value". Must be of type array or object implementing the \JsonSerializable interface.');
