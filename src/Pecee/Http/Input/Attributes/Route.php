409566fcdfeb31054bfd90448fa42dae2ad3ecf9
DeveloperMarius <info@marius-karstedt.de>
2023-12-17 14:50:09 +0100

    added title, description and request content type

--- src/Pecee/Http/Input/Attributes/Route.php
+++ src/Pecee/Http/Input/Attributes/Route.php
@@ -4,6 +4,7 @@ namespace Pecee\Http\Input\Attributes;
 
 use Attribute;
 use JetBrains\PhpStorm\ExpectedValues;
+use Pecee\Http\Request;
 
 /**
  *
@@ -26,11 +27,17 @@ class Route
      * @param string $method
      * @param string $route
      * @param array|null $settings
+     * @param string|null $title
+     * @param string|null $description
+     * @param string $content_type
      */
     public function __construct(
         #[ExpectedValues([Route::GET, Route::POST, Route::PUT, Route::PATCH, Route::DELETE, Route::OPTIONS])] private string $method,
         private string $route,
-        private ?array $settings = null
+        private ?array $settings = null,
+        private ?string $title = null,
+        private ?string $description = null,
+        #[ExpectedValues([Request::CONTENT_TYPE_JSON, Request::CONTENT_TYPE_FORM_DATA, Request::CONTENT_TYPE_X_FORM_ENCODED, 'text/plain'])] private string $content_type = Request::CONTENT_TYPE_JSON
     ){}
 
     /**
@@ -52,7 +59,32 @@ class Route
     /**
      * @return array|null
      */
-    public function getSettings(): ?array{
+    public function getSettings(): ?array
+    {
         return $this->settings;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContentType(): string
+    {
+        return $this->content_type;
     }
 }
